938708589995fa4df5d2c2f2efac299ce8b47d19
eaw4562 <eaw4562@naver.com>
2023-04-03 22:00:04 +0900

    네트워크 버튼 구현 완료 To-do : api 사용 데이터 맵핑

--- app/src/main/java/com/example/opusm/MainActivity.kt
+++ app/src/main/java/com/example/opusm/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.opusm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.*
+import android.widget.Button
+import android.widget.PopupWindow
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var popupWindow: PopupWindow
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,5 +20,25 @@ class MainActivity : AppCompatActivity() {
         transaction.replace(R.id.fragment_container, MainFragment())
         transaction.commit()
 
+        val testButton: Button = findViewById(R.id.btn_network)
+        testButton.setOnClickListener {
+            showNetworkDropdown(testButton)
+        }
     }
+
+    private fun showNetworkDropdown(anchorView: View) {
+        val layoutInflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val popupView = layoutInflater.inflate(R.layout.network_dropdown_layout, null)
+
+        popupWindow = PopupWindow(popupView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, true)
+
+        popupWindow.isOutsideTouchable = true
+        popupWindow.isTouchable = true
+        popupWindow.isFocusable = true
+
+        val location = IntArray(2)
+        anchorView.getLocationInWindow(location)
+        popupWindow.showAtLocation(anchorView, Gravity.NO_GRAVITY, 0, location[1] + anchorView.height)
+    }
+
 }
