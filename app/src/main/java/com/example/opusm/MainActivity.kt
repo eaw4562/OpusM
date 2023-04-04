769ab0ee6295827f61a00e23964ff021cddc4b79
myoung97 <ehaudwns97@gmail.com>
2023-04-04 17:59:53 +0900

    Room 구현 완료 -> 계정 정보 저장 작업중

--- app/src/main/java/com/example/opusm/MainActivity.kt
+++ app/src/main/java/com/example/opusm/MainActivity.kt
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.*
 import android.widget.Button
+import android.widget.ListView
 import android.widget.PopupWindow
 import androidx.appcompat.widget.AppCompatImageButton
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.example.opusm.dto.AccountInfo
+import com.google.android.material.navigation.NavigationView
+import com.example.opusm.adapter.AccountInfoAdapter
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var popupWindow: PopupWindow
+    private val accountList = mutableListOf<AccountInfo>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,13 +33,26 @@ class MainActivity : AppCompatActivity() {
             showNetworkDropdown(testButton)
         }
 
+        //네비게이션 드로어
         val drawLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
         val headerProfile = findViewById<AppCompatImageButton>(R.id.header_profile)
 
         headerProfile.setOnClickListener {
             drawLayout.openDrawer(GravityCompat.END)
         }
+
+        // 네비게이션 리스트뷰 초기화
+        val headerView = findViewById<NavigationView>(R.id.navigation_view).getHeaderView(0)
+        val listView = headerView.findViewById<ListView>(R.id.account_list_view)
+        val accountList = mutableListOf<AccountInfo>(
+            AccountInfo("계정1", "100 ETH", R.drawable.logo),
+            AccountInfo("계정2", "50 ETH", R.drawable.download),
+            AccountInfo("계정3", "20 ETH", R.drawable.wallet)
+        )
+        val adapter = AccountInfoAdapter(this, accountList)
+        listView.adapter = adapter
     }
+
 
 
     private fun showNetworkDropdown(anchorView: View) {
